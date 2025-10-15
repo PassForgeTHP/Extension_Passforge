731e37d90c31be532ba12aaf4a3a732c1dd76491
Paarroo <code_source_0@protonmail.com>
2025-10-15 14:09:38 +0200

    Add sort functionality for passwords

--- entrypoints/popup/components/PasswordList.jsx
+++ entrypoints/popup/components/PasswordList.jsx
@@ -1,6 +1,15 @@
+import { useState } from 'react';
 import PasswordItem from './PasswordItem';
 
 function PasswordList({ passwords, copiedId, onCopyUsername, onCopyPassword }) {
+  const [sortOrder, setSortOrder] = useState('newest');
+
+  const sortedPasswords = [...passwords].sort((a, b) => {
+    const dateA = new Date(a.created_at || 0);
+    const dateB = new Date(b.created_at || 0);
+    return sortOrder === 'newest' ? dateB - dateA : dateA - dateB;
+  });
+
   if (passwords.length === 0) {
     return (
       <div className="password-list">
@@ -13,7 +22,7 @@ function PasswordList({ passwords, copiedId, onCopyUsername, onCopyPassword }) {
 
   return (
     <div className="password-list">
-      {passwords.map(pwd => (
+      {sortedPasswords.map(pwd => (
         <PasswordItem
           key={pwd.id}
           password={pwd}
